d7c3fbf27f0118574a8d47ca4206525d1edeb412
Багаев Игорь Олегович <ibagaev@nur.briogroup.ru>
2025-09-30 19:03:28 +0300

    add files

---
+++ Test_Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
